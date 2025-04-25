24db3d2fbd808eec5adb1226f36de1dec3a2b01e
Andyfer004 <99309349+Andyfer004@users.noreply.github.com>
2025-04-24 20:41:01 -0600

    fix: version node change

--- dist/routes/openai.js
+++ dist/routes/openai.js
@@ -3,8 +3,6 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
-console.log("API KEY:", process.env.OPENAI_API_KEY, process.env.OPENAI_API_KEY.length);
-
 const openai = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY
 });
